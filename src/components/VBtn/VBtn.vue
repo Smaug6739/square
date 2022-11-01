0583a91a7dd551095b68823bd5783e26491e5a6c
Veauville Raphael <veauville.raphael@gmail.com>
2022-11-01 14:01:28 +0100

    Update

---
+++ src/components/VBtn/VBtn.vue
@@ -0,0 +1,12 @@
+<template>
+	<p>
+		Test
+	</p>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'VBtn',
+});
+</script>
---
+++ src/components/VBtn/__test__/VBtn.spec.ts
@@ -0,0 +1,11 @@
+import VBtn from '../VBtn.vue';
+console.log(VBtn);
+
+/*
+describe('VBtn.vue', () => {
+  it('renders a button', () => {
+    const wrapper = mount(VBtn);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
+*/

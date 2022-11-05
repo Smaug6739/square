7867df169d42c10fa1f30b5e60f7e7a361b7bdf2
Veauville Raphael <veauville.raphael@gmail.com>
2022-11-05 18:44:46 +0100

    Update tests

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,40 +1,18 @@
 <script setup lang="ts">
-import { VBtn } from "../../"
-function handleClick() {
-  console.log("Click")
-}
+import Components from './Components.vue'
 </script>
 
 <template>
   <main>
-    <div>
-      <h2>Buttons: Classic</h2>
-      <VBtn :onClick="() => handleClick()" primary>Primary</VBtn>
-      <VBtn :onClick="() => handleClick()" secondary>Secondary</VBtn>
-      <VBtn :onClick="() => handleClick()" danger>Danger</VBtn>
-      <VBtn :onClick="() => handleClick()" success>Success</VBtn>
-      <VBtn :onClick="() => handleClick()" warning>Warning</VBtn>
-      <VBtn :onClick="() => handleClick()" info>Info</VBtn>
-    </div>
-    <div>
-      <h2>Buttons: Transparent</h2>
-      <VBtn transparent primary>Primary</VBtn>
-      <VBtn transparent secondary>Secondary</VBtn>
-      <VBtn transparent danger>Danger</VBtn>
-      <VBtn transparent success>Success</VBtn>
-      <VBtn transparent warning>Warning</VBtn>
-      <VBtn transparent info>Info</VBtn>
-    </div>
-    <div>
-      <h2>Buttons: Disabled</h2>
-      <VBtn disabled primary>Primary</VBtn>
-      <VBtn disabled secondary>Secondary</VBtn>
-      <VBtn disabled danger>Danger</VBtn>
-      <VBtn disabled success>Success</VBtn>
-      <VBtn disabled warning>Warning</VBtn>
-      <VBtn disabled info>Info</VBtn>
-    </div>
+    <section id="light">
+      <Components />
+
+    </section>
+    <section id="dark">
+      <Components />
+    </section>
   </main>
+
 </template>
 
 <style>
@@ -53,9 +31,19 @@ body {
   margin: 0;
 }
 
-div {
-  margin-top: 15px;
-  padding: 10px;
+
+section {
+  min-height: 100vh;
+}
+
+#light {
+  background-color: #f5f5f5;
+  color: #212121;
+}
+
+#dark {
+  background-color: #212121;
+  color: #f5f5f5;
 }
 </style>
 
---
+++ test/src/Components.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { VBtn } from "../../"
+function handleClick() {
+	console.log("Click")
+}
+</script>
+
+<template>
+	<main>
+		<div>
+			<h2>Buttons: Classic</h2>
+			<VBtn :onClick="() => handleClick()" primary>Primary</VBtn>
+			<VBtn :onClick="() => handleClick()" secondary>Secondary</VBtn>
+			<VBtn :onClick="() => handleClick()" danger>Danger</VBtn>
+			<VBtn :onClick="() => handleClick()" success>Success</VBtn>
+			<VBtn :onClick="() => handleClick()" warning>Warning</VBtn>
+			<VBtn :onClick="() => handleClick()" info>Info</VBtn>
+		</div>
+		<div>
+			<h2>Buttons: Transparent</h2>
+			<VBtn transparent primary>Primary</VBtn>
+			<VBtn transparent secondary>Secondary</VBtn>
+			<VBtn transparent danger>Danger</VBtn>
+			<VBtn transparent success>Success</VBtn>
+			<VBtn transparent warning>Warning</VBtn>
+			<VBtn transparent info>Info</VBtn>
+		</div>
+		<div>
+			<h2>Buttons: Disabled</h2>
+			<VBtn disabled primary>Primary</VBtn>
+			<VBtn disabled secondary>Secondary</VBtn>
+			<VBtn disabled danger>Danger</VBtn>
+			<VBtn disabled success>Success</VBtn>
+			<VBtn disabled warning>Warning</VBtn>
+			<VBtn disabled info>Info</VBtn>
+		</div>
+	</main>
+</template>
+
+<style scoped>
+div {
+	margin-top: 15px;
+	padding: 10px;
+}
+</style>
+
